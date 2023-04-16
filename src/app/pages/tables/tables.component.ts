36c99d925bd330ec7ebd4221850436348cc0329a
DESKTOP-311EKJF\andrew <ramirezandrew172@gmail.com>
2023-04-16 00:36:50 -0500

    add chart

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "../../variables/charts";
 
 @Component({
   selector: 'app-tables',
@@ -9,10 +17,40 @@ import { Component, OnInit } from '@angular/core';
 export class TablesComponent implements OnInit {
 
   activeTab: string = 'Presions Arterial';
+  public datasets: any;
+  public data: any;
+  public salesChart;
+  public clicked: boolean = true;
+  public clicked1: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.datasets = [
+      [0, 20, 10, 30, 15, 40, 20, 60, 60],
+      [0, 20, 5, 25, 10, 30, 15, 40, 40]
+    ];
+    this.data = this.datasets[0];
+
+
+    var chartOrders = document.getElementById('chart-sales');
+
+    parseOptions(Chart, chartOptions());
+
+
+    var ordersChart = new Chart(chartOrders, {
+      type: 'bar',
+      options: chartExample2.options,
+      data: chartExample2.data
+    });
+
+    var chartSales = document.getElementById('chart-sales');
+
+    this.salesChart = new Chart(chartSales, {
+			type: 'line',
+			options: chartExample1.options,
+			data: chartExample1.data
+		});
   }
 
   onTabClick(tab) {
